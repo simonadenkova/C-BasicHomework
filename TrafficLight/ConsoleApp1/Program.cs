90f71c3e8b78efe33b612776363d4dd415fcfc52
simonadenkova <59212001+simonadenkova@users.noreply.github.com>
2020-03-04 23:42:07 +0100

    Add files via upload

---
+++ TrafficLight/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("STOP");
+            Thread.Sleep(1500);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("READY");
+            Thread.Sleep(1500);
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO!");
+
+            Console.ReadLine();
+
+
+
+            
+        }
+    }
+}
